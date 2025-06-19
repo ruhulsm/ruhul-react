21a075c5608755f792a1c926c6b96966db8a7245
S. M. Ruhul Amin <work.ruhulamin@gmail.com>
2025-06-19 23:53:03 +0600

    Updated Vite React project code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
   return (
     <>
-      <h2>Hello World</h2>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
-  )
+  );
 }
-
-export default App
+export default ColorSchemesExample;
